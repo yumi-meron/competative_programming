7af129a0bc0fc072f05358bd01a6ee0dc0862b73
meron weldu <meron.w.teka@gmail.com>
2023-06-07 21:10:28 +0300

    Time: 497 ms (35.59%), Space: 29.4 MB (11.39%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(set(nums)) != len(nums):
+            return True
+       
+            
+                
+            
+        
