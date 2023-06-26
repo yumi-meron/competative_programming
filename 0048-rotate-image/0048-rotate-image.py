9405c08d4c8352b27d5852636f4cec34637e18d0
meron weldu <meron.w.teka@gmail.com>
2023-06-26 15:57:32 +0200

    Time: 19 ms (77.76%), Space: 13.2 MB (81.40%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        for i in range(len(matrix)):
+            for j in range(i+1, len(matrix[0])):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for x in matrix:
+            x.reverse()
+        return matrix
