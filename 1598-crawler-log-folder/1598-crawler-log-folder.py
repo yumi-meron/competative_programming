f82a7112af6136403c2c402748a7040ed998ef0f
meron weldu <meron.w.teka@gmail.com>
2023-08-17 20:34:08 +0300

    Time: 64 ms (39.89%), Space: 16.5 MB (76.88%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -2,12 +2,9 @@ class Solution:
     def minOperations(self, logs: List[str]) -> int:
         depth = 0
         for i in logs:
-            if i == "./":
-                continue
-            elif i == "../":
-                depth -= 1
-                if depth<0:
-                    depth = 0
-            else:
+            if i == "../":
+                depth = max(0, depth-1)
+                
+            elif i != "./":
                 depth+=1
         return depth
