c13c67db7a189088a1b0e2227a4818a3500b4fea
meron weldu <meron.w.teka@gmail.com>
2023-08-18 10:14:00 +0300

    Time: 46 ms (46.60%), Space: 16.4 MB (12.73%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        lookup = {')':'(', ']':'[', '}':'{'}
+        for i in s:
+            if i in lookup.values():
+                stack.append(i)
+            elif stack and lookup[i] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+        return stack == []
+            
+        
+        
