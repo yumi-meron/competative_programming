32234e0265ac90f282ca85fc0d30796300116baf
meron weldu <meron.w.teka@gmail.com>
2023-08-19 14:43:20 +0300

    Time: 34 ms (89.66%), Space: 16.1 MB (99.73%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,14 +1,12 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        stack = [0]
-        for i in s:
-            if i == '(':
-                stack.append(0)
+        l = 0
+        score = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                l +=1
             else:
-                val = stack.pop()
-                stack[-1] += max(2*val, 1)
-                
-        return stack.pop()
-                
-                
-        
+                l-=1
+                if s[i-1] == '(':
+                    score += 2**(l)
+        return score
