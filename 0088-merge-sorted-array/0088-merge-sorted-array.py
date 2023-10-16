8828cd5c167d6c4a0a2e1bb6bf373c48e3499442
meron weldu <meron.w.teka@gmail.com>
2023-10-16 22:44:40 +0200

    Time: 35 ms (94.84%), Space: 16.2 MB (91.38%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,21 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = j = 0
-        while i<(n+m) and j<n:
-            if i >=j+m:
-                nums1.insert(i,nums2[j])
-                nums1.pop()
-                i+=1
-                j+=1
+        i = m-1
+        j=n-1
+        ind = n+m-1
+        while j>=0:
+            if i>=0 and nums2[j]<nums1[i]:
+                nums1[ind] = nums1[i]
+                i-=1
             else:
-                if nums1[i]>=nums2[j]:
-                    nums1.insert(i,nums2[j])
-                    nums1.pop()
-                    j+=1
-                    i+=1
-                else:
-                    i+=1
+                nums1[ind] = nums2[j]
+                j-=1
+            ind-=1
+            
+            
             
         
             
