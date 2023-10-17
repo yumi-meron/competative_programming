2b938a6881e13c1653d6c5f61b8ee9f4f3749404
meron weldu <meron.w.teka@gmail.com>
2023-10-17 23:01:48 +0200

    moving files

--- progress sheet/1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ progress sheet/1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,16 +1,18 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        i,j = 0,0
         merged = ""
-        while i<len(word1) or j<len(word2):
-            if i<len(word1):
-                merged+=word1[i]
-            if j<len(word2):
-                merged+=word2[j]
+        i = 0
+        j=0
+        while i<len(word1) and j<len(word2):
+            merged += word1[i]
+            merged += word2[j]
             i+=1
             j+=1
+        if i<len(word1):
+            merged += word1[i:]
+        elif j<len(word2):
+            merged += word2[j:]
         return merged
-
             
         
         
