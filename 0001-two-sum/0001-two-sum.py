66ef0c15f8a5f26110534e9f40c3865288439c8b
meron weldu <meron.w.teka@gmail.com>
2023-10-28 10:23:47 +0200

    Time: 34 ms (89.87%), Space: 14.2 MB (21.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,13 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
-        
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j]== target:
-                    return [i,j]
-                    break
-
-        
+        dic = {}
+        for i, element in enumerate(nums):
+            x = target - element
+            if x in dic:
+                return [dic[x],i]
+            dic[element] = i
+        return []
         
