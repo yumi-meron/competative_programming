7224ea68ad32507c0aac4656c60059ef1de70483
meron weldu <meron.w.teka@gmail.com>
2023-11-29 17:23:14 +0100

    moving files

--- progress sheet/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ progress sheet/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,8 +1,17 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        now = best = sum(nums[:k])
+        #[1,12,-5,-6,50,3]
+        #[1,12,-5,-6]
+        #[12,-5,-6,50]
+        #[-5,-6,50,3]
+        max_val = now = sum(nums[:k])
         for i in range(k,len(nums)):
-            now += nums[i] - nums[i-k]
-            if now>best:
-                best = now
-        return best/k
+            now +=  nums[i] - nums[i-k]
+            if now>max_val: max_val = now
+            
+        return max_val / k
+            
+            
+            
+        
+        
