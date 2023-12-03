289ec47c0a76c74d3e9f7a1f19ce6747158bdbf1
meron weldu <meron.w.teka@gmail.com>
2023-12-03 12:50:37 +0100

    Added solution for Decrypt String from Alphabet to Integer Mapping

---
+++ activities/week1/decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,13 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        ans = []
+        i = len(s)-1
+        while i>=0:
+            if s[i]=='#':
+                ans.append(chr(int(s[i-2:i])+96))
+                i-=3
+            else:
+                ans.append(chr(int(s[i])+96))
+                i-=1
+        return ''.join(ans[::-1])
+            
