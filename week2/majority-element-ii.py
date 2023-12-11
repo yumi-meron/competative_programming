65a8827c667e9f4be80218d9f52c756fb65dc203
meron weldu <meron.w.teka@gmail.com>
2023-12-11 18:02:27 +0100

    Added solution for Majority Element II

--- week2/majority-element-ii.py
+++ week2/majority-element-ii.py
@@ -3,12 +3,15 @@ class Solution:
         # dic is dictionary to store the number of apperance of all elements
         dic = Counter(nums)
         n = len(nums) // 3 # n is the min apperance needed to consider an element as an answer
-        ans = []
+        
+        ans = filter(lambda x: dic[x] > n ,dic)
 
-        for key in dic.keys():
-            if dic[key] > n:
-                ans.append(key)
-                
         return ans
+
+        # for key in dic.keys():
+        #     if dic[key] > n:
+        #         ans.append(key)
+                
+        
 
         
