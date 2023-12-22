59939bc7f7e16b0196d266a4fb0d1af1a2b5d01d
meron weldu <meron.w.teka@gmail.com>
2023-12-22 17:32:12 +0100

    Added solution for Transpose Matrix

--- week2/transpose-matrix.py
+++ week2/transpose-matrix.py
@@ -1,7 +1,10 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        ans = list(zip(*matrix))
-        return ans
+        transposed = [[0]*len(matrix) for clm in range(len(matrix[0]))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                transposed[j][i] = matrix[i][j]
+        return transposed
 
             
         
