da5867f0647e676c2a12c120fec931d2174c9e4a
meron weldu <meron.w.teka@gmail.com>
2023-12-26 17:24:16 +0100

    Added solution for Sort the People

--- week2/sort-the-people.py
+++ week2/sort-the-people.py
@@ -1,23 +1,23 @@
-class Solution(object):
-    def sortPeople(self, names, heights):
-        """
-        :type names: List[str]
-        :type heights: List[int]
-        :rtype: List[str]
-        """
-        for i in range(len(heights)):
-            max = heights[i]
-            pos = i
-            for j in range(i+1,len(heights)):
-                if heights[j] > max:
-                    max = heights[j]
-                    pos = j
-            heights[i], heights[pos] = heights[pos], heights[i]
-            names[i], names[pos] = names[pos], names[i]
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        num = max(heights)
+        lst = [0 for i in range(num+1)]
+
+        ans = []
+        for i in range(len(names)):
+            lst[heights[i]] = names[i]
+       
+        for j in range(num,-1,-1):
+            if lst[j] != 0:
+                ans.append(lst[j])
+        return ans
+
             
-        return names
-        
-            
-                
-            
+
+        # for i in range(len(names)):
+        #     for j in range(i+1,len(names)):
+        #         if heights[i] < heights[j]:
+        #             heights[i], heights[j] = heights[j], heights[i]
+        #             names[i], names[j] = names[j], names[i]
+        # return names
         
