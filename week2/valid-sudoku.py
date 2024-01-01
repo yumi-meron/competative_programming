e0e11ab3daa77df4a57ce752daa6099e88a12e94
meron weldu <meron.w.teka@gmail.com>
2024-01-01 11:47:11 +0100

    Added solution for Valid Sudoku

--- week2/valid-sudoku.py
+++ week2/valid-sudoku.py
@@ -1,26 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        boxes = [set() for _ in range(9)]
-
+        myset = set()
         for i in range(9):
             for j in range(9):
                 val = board[i][j]
                 if val == '.':
                     continue
-
-                if val in rows[i]:
-                    return False
-                rows[i].add(val)
-
-                if val in cols[j]:
-                    return False
-                cols[j].add(val)
-
-                idx = (i // 3) * 3 + j // 3
-                if val in boxes[idx]:
-                    return False
-                boxes[idx].add(val)
-
+                else:
+                    row = (i,val)
+                    col = (val, j)
+                    box = (i//3,j//3,val)
+                    if row in myset or col in myset or box in myset:
+                        return False
+                    myset.add(row)
+                    myset.add(col)
+                    myset.add(box)
         return True
