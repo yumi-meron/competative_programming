2611ecd85d5a1c95b36558fde14cae3ecf24eb2a
meron weldu <meron.w.teka@gmail.com>
2024-02-23 02:04:14 +0100

    Add solution for Daily Temperatures

--- week2/leetcode/daily-temperatures.py
+++ week2/leetcode/daily-temperatures.py
@@ -3,14 +3,20 @@ class Solution:
         stack = []
         ans = [0] * len(temper)
 
-        for i,temp in reversed(list(enumerate(temper))):
+        # for i,temp in reversed(list(enumerate(temper))):
 
-            while stack and temper[stack[-1]] <= temp:
-                stack.pop()
+        #     while stack and temper[stack[-1]] <= temp:
+        #         stack.pop()
 
-            if stack:
-                ans[i] = stack[-1] - i
+        #     if stack:
+        #         ans[i] = stack[-1] - i
                 
+        #     stack.append(i)
+        for i,temp in enumerate(temper):
+            while stack and temper[stack[-1]] < temp:
+                prev = stack.pop()
+                ans[prev] = i - prev
+
             stack.append(i)
             
         return ans
