98edfee3e786cb9ff0ef424e5a23c9c31820bd38
meron weldu <meron.w.teka@gmail.com>
2024-03-06 13:23:54 +0100

    Add solution for Search Insert Position

---
+++ week2/leetcode/search-insert-position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left,right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid-1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+            # if left == right:
+            #     return left
+        return left
